9a12f75cf04048ffe0aa083405b0f6330f861ba6
tienysusanattukal <80477363+tienysusanattukal@users.noreply.github.com>
2023-09-16 09:50:01 +0530

    raising 404 error using try except method

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns =[
-    path('',views.index)
+    path('',views.index,name='movies_index'),
+    path('<int:movie_id>',views.detail,name="movie_detail")
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Movie
 # Create your views here.
 def index(request):
     mov = Movie.objects.all()
     
     return render(request,'movies/index.html',{'movi':mov})
+
+def detail(request,movie_id):
+    try:
+        mo = Movie.objects.get(pk=movie_id)
+        return render(request,'movies/detail.html',{'moo':mo})
+    except Movie.DoesNotExist:
+        raise Http404()
+
