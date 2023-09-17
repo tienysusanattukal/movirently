1c03767d71ddba77faf531340a6a7f24c91a393b
tienysusanattukal <80477363+tienysusanattukal@users.noreply.github.com>
2023-09-17 20:34:33 +0530

    commit after adding allowed hosts

--- movrently/settings.py
+++ movrently/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [https://movrently-8psgdbevc-tienysusanattukal.vercel.app/]
+ALLOWED_HOSTS = ['https://movrently-8psgdbevc-tienysusanattukal.vercel.app/']
 
 
 # Application definition
