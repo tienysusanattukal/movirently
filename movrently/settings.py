25443dec8a1647c1caedcb071e264c50f9e8dd60
tienysusanattukal <80477363+tienysusanattukal@users.noreply.github.com>
2023-09-17 14:56:41 +0530

    debug set to false but allowedhost not added

--- movrently/settings.py
+++ movrently/settings.py
@@ -25,7 +25,7 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
