661efa6def8ea9b70a66f0c6f5ef501ad73e64bc
tienysusanattukal <80477363+tienysusanattukal@users.noreply.github.com>
2023-09-17 10:29:47 +0530

    url tags used to map urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
-
+#app_name ="movies"
 urlpatterns =[
     path('',views.index,name='movies_index'),
-    path('<int:movie_id>',views.detail,name="movie_detail")
+    path('<int:movie_id>',views.detail,name="movies_detail")
 ]
